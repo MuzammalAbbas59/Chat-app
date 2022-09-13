d9a6c8de639cf2046634bb5eca8ec4baf11ab16e
Muzammalabbas59 <abmuzammal17@gmail.com>
2022-09-13 19:12:10 +0500

    add message partial

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    has_many :messages
+    validates :username, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 4, maximum: 20 }
+has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"ahsan", password:"abbasboy")
+User.create(username:"khuram", password:"abbasboy")
+User.create(username:"hayayt", password:"abbasboy")
